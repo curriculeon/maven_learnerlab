00d9d902499080d468a9b20987807ecba9c78ed6
santa-js-html <sanafaris2000@yahoo.com>
2020-01-08 14:35:35 -0500

    Done 1-08-2020

--- src/test/java/com/github/curriculeon/TestClassroom.java
+++ src/test/java/com/github/curriculeon/TestClassroom.java
@@ -8,13 +8,11 @@ import java.lang.reflect.Array;
 public class TestClassroom {
     @Test
     public void testHostLecture(){
-        String[] names = {"Abbott", "Apollo", "Armani", "Aristotle", "Arrow"};
-        long[] id={1,2,3,4,5};
         String actual = "true";
         double numberOfHours =5;
         Teacher teacher = new Instructor(10, "Jaba");
         Classroom classroom = new Classroom();
-        double[] total = new double[names.length];
+        double[] total = new double[classroom.students.personList.size()];
         for(int i=0; i< classroom.students.personList.size(); i++){
             Student stu = classroom.students.personList.get(i);
             total[i]=  stu.totalStudyTime+ numberOfHours;
@@ -24,6 +22,27 @@ public class TestClassroom {
             Student stu = classroom.students.personList.get(i);
             if(stu.totalStudyTime != total[i]){
                actual ="false";
+            }
+        }
+
+        Assert.assertTrue(actual, true);
+    }
+    @Test
+    public void testHostLectureEducator(){
+        String actual = "true";
+        double numberOfHours =5;
+        Educator educator = Educator.Apollo;
+        Classroom classroom = new Classroom();
+        double[] total = new double[classroom.students.personList.size()];
+        for(int i=0; i< classroom.students.personList.size(); i++){
+            Student stu = classroom.students.personList.get(i);
+            total[i]=  stu.totalStudyTime+ numberOfHours;
+        }
+        classroom.hostLecture(educator, numberOfHours);
+        for(int i=0; i< classroom.students.personList.size(); i++){
+            Student stu = classroom.students.personList.get(i);
+            if(stu.totalStudyTime != total[i]){
+                actual ="false";
             }
         }
 
