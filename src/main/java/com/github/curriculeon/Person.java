16abbc71c7068992e760fcd8995659cd8e67717b
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-07-20 11:34:41 -0400

    Inital work of part 1

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,5 +1,24 @@
 package com.github.curriculeon;
 
 public class Person {
+    private final long id;
+    private String name;
+
+    Person(long i, String s){
+        id = i;
+        name = s;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
