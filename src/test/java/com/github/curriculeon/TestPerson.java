9ad376af4304846fd3391668701d7fc76aa57a55
suresh kandagatla <surikandagatla@gmail.com>
2020-07-20 11:06:40 -0400

    Test class partially done

--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,21 @@
 package com.github.curriculeon;
 
-public class TestPerson {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestPerson {
+    @Test
+    public void testConstructor(long expectedId, String expectedName) {
+        Person person = new Person(expectedId, expectedName);
+        long actualId = person.getId();
+        String actualName = person.getName();
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void testSetName(String name) {
+
+        person.setName(name);
+    }
 }
