498bcbd895ba1428c92d9be6f8ba866a1c4fe6e3
Alonso Ninalaya <pikachualonso@gmail.com>
2020-07-20 15:47:35 -0400

    changes added

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,5 +1,23 @@
 package com.github.curriculeon;
 
 public class Person {
+    private final long id;
+    private String name;
 
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
