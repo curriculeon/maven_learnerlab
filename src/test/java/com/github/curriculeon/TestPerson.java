a5bb44859fa1b03acecce55757784c9dfb98cb11
Marcus Katalenas <katalenasm2@mailbx.winthrop.edu>
2020-07-20 14:39:18 -0400

    Created my first test case ever using maven and passed successfully

--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,20 @@
 package com.github.curriculeon;
-
+import org.junit.Assert;
+import org.junit.Test;
 public class TestPerson {
 
+    private void test(long expectedLong, String expectedString){
+        Person person = new Person(expectedLong, expectedString);
+
+        long actualLong = person.getId();
+        String actualString = person.getName();
+
+        Assert.assertEquals(expectedLong, actualLong);
+        Assert.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void test0(){
+        test(1, "Marcus");
+    }
 }
