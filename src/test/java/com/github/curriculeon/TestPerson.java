f7ef1c901ff93c5f62332a86f9e30cd7f91814e7
David Frary <fraryd@gmail.com>
2020-07-20 15:48:37 -0400

    finished edits on 'TestPerson.java'

--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,24 @@
 package com.github.curriculeon;
 
 public class TestPerson {
+    private void testConstructor(long expectedId, String expectedName) {
+        Person person = new Person(expectedId, expectedName);
+        Long actualId = person.getId();
+        String actualName = person.getName();
 
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedName, actualName());
+
+    }
+
+    private void testSetName() {
+        Person person = new Person(id:null, name: null);
+        String expected Name = "Some name";
+
+        person.setName(expectedName);
+        String actualName = person.getName();
+
+        Assert.assertEquals(expectedName, actualName());
+
+    }
 }
