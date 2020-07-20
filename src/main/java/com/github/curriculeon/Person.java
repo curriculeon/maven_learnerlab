c34e07306bf9b30e56b4c7644d42d79644c8116d
Nkyo Lio <nkyoli@gmail.com>
2020-07-20 10:44:26 -0400

    Last update

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,5 +1,27 @@
 package com.github.curriculeon;
 
 public class Person {
+    private final long id;
+    private String name;
 
+    public Person() {
+        this(0, "");
+    }
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
