1e5cac16444919432196f5f82c2fb6a6913a3869
mondira-roy2020 <mondira.roy.git2020@gmail.com>
2020-07-20 11:00:09 -0400

    Person class created

---
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -0,0 +1,7 @@
+package com.github.curriculeon;
+
+public class MainApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/groovy/util/GroovyTestCase.groovy
@@ -0,0 +1,4 @@
+package groovy.util
+
+class GroovyTestCase {
+}
