171a6c6b7d2925dbef58d4a7610e1790062079f0
AnjaliG927 <anjali.ghaligi@gmail.com>
2020-07-21 10:23:41 -0400

    part1

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,5 +1,23 @@
 package com.github.curriculeon;
 
 public class Person {
+    final Long id;
+    String name;
 
+    public Person(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
