ac9731a509a99c68cc538fd62b8e38da083d9e05
razeroual <47251928+razeroual@users.noreply.github.com>
2020-07-21 12:07:48 -0400

    create a student class

---
+++ src/main/java/com/github/curriculeon/Student.java
@@ -0,0 +1,20 @@
+package com.github.curriculeon;
+
+public class Student extends Person implements Learner {
+    private Double totalStudyTime;
+
+    public Student(Long id, String name, Double totalStudyTime) {
+        super(id, name);
+        this.totalStudyTime = totalStudyTime;
+    }
+
+    @Override
+    public void learn(double numberOfHours) {
+        this.totalStudyTime = this.totalStudyTime + numberOfHours;
+    }
+
+    @Override
+    public Double getTotalStudyTime() {
+        return  this.totalStudyTime;
+    }
+}
