4786cb80d4e6bd82d9d2820b842df4db00ffe717
suresh kandagatla <surikandagatla@gmail.com>
2020-07-21 15:40:54 -0400

    Made changes to part 3.0

--- src/test/java/com/github/curriculeon/TestStudent.java
+++ src/test/java/com/github/curriculeon/TestStudent.java
@@ -1,30 +1,45 @@
 package com.github.curriculeon;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestStudent {
     @Test
     public void testImplementation() {
-        long expectedId = 123456789;
-        String expectedName= "Some name";
-        double expectedStudyTime = 6;
-       Student student =  new Student(expectedId, expectedName);
-       student.learn(expectedStudyTime);
-       double actualStudyTime = student.getTotalStudyTime();
-        Assert.assertEquals(expectedStudyTime, actualStudyTime,123456789);
+//        long expectedId = 123456789;
+//        String expectedName= "Some name";
+//        double expectedStudyTime = 6;
+//       Student student =  new Student(expectedId, expectedName);
+//       student.learn(expectedStudyTime);
+//       double actualStudyTime = student.getTotalStudyTime();
+//        Assert.assertEquals(expectedStudyTime, actualStudyTime,123456789);
+        //given
+        Student student = new Student(0, null);
+
+        //when
+        Boolean result = student instanceof Learner;
+        //then
+        Assert.assertTrue(result);
     }
 
     @Test
     public void testInheritance() {
-        long expectedId = 123456789;
-        String expectedName = "John";
-        Student student = new Student(expectedId, expectedName);
-        long actualId = student.getId();
-        String actualName= student.getName();
+//        long expectedId = 123456789;
+//        String expectedName = "John";
+//        Student student = new Student(expectedId, expectedName);
+//        long actualId = student.getId();
+//        String actualName= student.getName();
+//
+//        Assert.assertEquals(expectedId, actualId);
+//        Assert.assertEquals(expectedName, actualName);
 
-        Assert.assertEquals(expectedId, actualId);
-        Assert.assertEquals(expectedName, actualName);
+        //given
+        Student student = new Student(0, null);
+        //when
+        Boolean result = student instanceof Person;
+        //then
+        Assert.assertTrue(result);
     }
 
     @Test
