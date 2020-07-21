00e6c6ce405febc2d4cd21dc3590d04f2b2c05f5
David Frary <fraryd@gmail.com>
2020-07-21 11:05:16 -0400

    finished edits on 'Person.java'

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,23 +1,23 @@
 package com.github.curriculeon;
 
 public class Person {
-    private final long id;
+    final long id;
     private String name;
 
-    public Person (long id, String name){
+    Person(long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public long getId(){
-        return this.id;
+    public long getId() {
+        return id;
     }
 
-    public String getName(){
-        return this.name;
+    public String getName() {
+        return name;
     }
 
-    public void setName (String name){
+    public void setName(String name) {
         this.name = name;
     }
 
