0d651f697e554625dea4d7ee5185e9e35af77853
luckyavailable <lkbulusu@gmail.com>
2020-07-21 21:29:24 -0400

    Learner assignment partly completed

--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,39 @@
 package com.github.curriculeon;
 
-public class TestPerson {
 
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestPerson {
+    @Test
+    public void testConstructor() {
+        // given
+        long expectedId = 0L;
+        String expectedName = "Some name";
+
+        // when
+        Person person = new Person(expectedId,expectedName);
+        long actualId = person.getid();
+        String actualName = person.getname();
+
+        // then
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedName, actualName);
+    }
+    @Test
+    public void testSetName() {
+        // given
+        Person person = new Person(0L, null);
+        String expectedName = "Testing";
+
+
+
+        // when
+        person.setname(expectedName);
+        String actualName = person.getname();
+
+        // then
+        Assert.assertEquals(expectedName, actualName);
+    }
 }
+
