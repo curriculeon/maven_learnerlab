8da2e8585961b36c7158cdd2a69e83f8808e0269
Steven Snyder <sts12589@gmail.com>
2020-07-23 15:56:15 -0400

    completed part 6.0 for testing

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -5,10 +5,15 @@ public class Person {
     private final Long id;
     private String name;
 
+    public Person() {
+        this.id = null;
+    }
+
     public Person(Long id, String name) {
         this.id = id;
         this.name = name;
     }
+
 
     public Long getId() {
         return id;
