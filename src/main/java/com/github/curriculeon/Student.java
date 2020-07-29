737feb3e37d7740b1455c5acb3bc6a56724958f1
Ezra Vance <3zravance@gmail.com>
2020-07-28 21:29:58 -0400

    Student class created.

---
+++ src/main/java/com/github/curriculeon/Student.java
@@ -0,0 +1,23 @@
+package com.github.curriculeon;
+/*
+    The Student class is a Person and can learn.
+    Student must call parent's constructor using keyword super if that constructor uses arguments to be instantiated.
+ */
+public class Student extends Person implements Learner{
+    private double totalStudyTime;
+
+    public Student(long id, String name) {
+        super(id, name);
+    }
+
+
+    @Override
+    public void learn(double numberOfHours) {
+        totalStudyTime += numberOfHours;
+    }
+
+    @Override
+    public Double getTotalStudyTime() {
+        return totalStudyTime;
+    }
+}
