2896ceb1503cda101e6031a7e1d357771595041d
Ezra Vance <3zravance@gmail.com>
2020-07-28 22:14:47 -0400

    Instructor class created.

---
+++ src/main/java/com/github/curriculeon/Instructor.java
@@ -0,0 +1,23 @@
+package com.github.curriculeon;
+/*
+    The Instructor class is a person and can teach.
+ */
+public class Instructor extends Person implements Teacher{
+
+    public Instructor(long id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void teach(Learner learner, double numberOfHours) {
+        learner.learn(numberOfHours);
+    }
+
+    @Override
+    public void lecture(Learner[] learners, double numberOfHours) {
+        double numberOfHoursPerLearner = numberOfHours / learners.length;
+        for (Learner learner : learners) {
+            learner.learn(numberOfHoursPerLearner);
+        }
+    }
+}
