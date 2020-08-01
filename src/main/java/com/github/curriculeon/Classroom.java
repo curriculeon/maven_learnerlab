48b00605c40fe2808258f2c9ca23ec69f88e46f7
unknown <dyirchott@hotmail.com>
2020-08-01 13:17:15 -0400

    progress before lunch; understanding more

--- src/main/java/com/github/curriculeon/Classroom.java
+++ src/main/java/com/github/curriculeon/Classroom.java
@@ -1,7 +1,17 @@
 package com.github.curriculeon;
 
 public class Classroom {
-    private Students students;
-    private Instructors instructors;
+    private Students students = Students.getInstance();
+    private Instructors instructors = Instructors.getInstance();
+
+    public void hostLecture(Teacher teacher, Double numberOfHours){
+        teacher.lecture((Learner[])students.toArray(), numberOfHours);
+    }
+
+    public void hostLecture(long id, Double numberOfHours) {
+        Person person = instructors.findById(id);
+        Instructor instructor = (Instructor)person;
+        instructor.lecture((Learner[])students.toArray(), numberOfHours);
+    }
 
 }
