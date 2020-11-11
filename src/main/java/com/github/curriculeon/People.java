34dc5f232d64b0ddf41799621506b51fa6fe0372
Trevis42 <sirtrevis42@gmail.com>
2020-11-11 09:52:42 -0600

    Finished adding methods to People

--- src/main/java/com/github/curriculeon/People.java
+++ src/main/java/com/github/curriculeon/People.java
@@ -1,14 +1,13 @@
 package com.github.curriculeon;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class People {
-    private List<Person> personList = new ArrayList<>();
+public class People implements Iterable<Person>{
+    private final List<Person> personList = new ArrayList<>();
 
-    public void add(Person person) {
-        personList.add(person);
-    }
+    public void add(Person person) { personList.add(person); }
 
     public  Person findById(Long id) {
         return personList
@@ -18,5 +17,28 @@ public class People {
                 .get();
     }
 
+    public Boolean contains(Person person) {
+        return personList.contains(person);
+    }
 
+    public void remove(Person person) { personList.remove(person); }
+
+    public void remove(Long id) {
+        personList.removeIf(person -> person.getId() == id);
+    }
+
+    public void removeAll() { personList.clear(); }
+
+    public Integer count() {
+        return personList.size();
+    }
+
+    public Object[] toArray(){
+        return personList.toArray();
+    }
+
+    @Override
+    public Iterator<Person> iterator() {
+        return personList.iterator();
+    }
 }
