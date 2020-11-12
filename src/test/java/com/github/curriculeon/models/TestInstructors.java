103a55c8db82413c5a7893ae784638ad01d6ea08
Neely Mann <NMann@talentpath.com>
2020-11-12 14:28:33 -0500

    fixed

--- src/test/java/com/github/curriculeon/models/TestInstructors.java
+++ src/test/java/com/github/curriculeon/models/TestInstructors.java
@@ -1,7 +1,5 @@
 package com.github.curriculeon.models;
 
-import com.github.curriculeon.interfaces.Learner;
-import com.github.curriculeon.interfaces.Teacher;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,13 +9,18 @@ import java.util.List;
 public class TestInstructors {
     @Test
     public void test() {
-        String[] nameArray = "Leon Hunter,Fred Zirdung,David Smelser".split(",");
+        // given
+        String[] nameArray = "Leon Hunter,Fred Zirdung,David S".split(",");
         List<String> nameList = Arrays.asList(nameArray);
 
-        for (Person person: Instructors.getInstance()) {
-            String personName = person.getName();
+        // when
 
+        Instructors instructorsSingleton = Instructors.getInstance();
+        for(Person person : instructorsSingleton) {
+            String personName = person.getName();
+            // then
             Assert.assertTrue(nameList.contains(personName));
         }
     }
+
 }
