a094ff780c377f4e3ae5441bd144469e6a12268e
AndrewAscone <andrew.ascone@gmail.com>
2023-08-24 14:32:25 -0400

    Created and tested Person class

--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,29 @@
 package com.github.curriculeon;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
 
+    @Test
+    public void testConstructor(){
+        Person test = new Person(1L, "Leon");
+
+        String expected = "Leon";
+        String actual = test.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetName(){
+        Person test = new Person(1L, "Leon");
+
+        test.setName("Noel");
+
+        String expected = "Noel";
+        String actual = test.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
