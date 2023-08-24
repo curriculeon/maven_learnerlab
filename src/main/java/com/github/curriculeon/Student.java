f9b7b8ed2676a1dc3b3c4c4f6d8ceee2aeee4b20
AndrewAscone <andrew.ascone@gmail.com>
2023-08-24 14:40:34 -0400

    Created student class

---
+++ src/main/java/com/github/curriculeon/Student.java
@@ -0,0 +1,20 @@
+package com.github.curriculeon;
+
+public class Student extends Person implements Learner {
+
+    double totalStudyTime;
+
+    public Student(long id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void learn(double numberOfHours) {
+        this.totalStudyTime += numberOfHours;
+    }
+
+    @Override
+    public double getTotalStudyTime() {
+        return this.totalStudyTime;
+    }
+}
