ea5d0a584c9cde82137338c1042b66379ce4e267
AndrewAscone <andrew.ascone@gmail.com>
2023-08-25 18:10:24 -0400

    Began creating Instructor class

---
+++ src/main/java/com/github/curriculeon/Instructor.java
@@ -0,0 +1,18 @@
+package com.github.curriculeon;
+
+public class Instructor extends Person implements Teacher {
+
+    public Instructor(long id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void teach(Learner learner, double numberOfHours) {
+        learner.learn(numberOfHours);
+    }
+
+    @Override
+    public void lecture(Learner[] learners, double numberOfHours) {
+
+    }
+}
