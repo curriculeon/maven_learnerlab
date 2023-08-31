c3640b11ae8ad6efb084f66610053bffc07b0b2b
AndrewAscone <andrew.ascone@gmail.com>
2023-08-31 15:00:10 -0400

    Created Educator enum

---
+++ src/main/java/com/github/curriculeon/Educator.java
@@ -0,0 +1,18 @@
+package com.github.curriculeon;
+
+public enum Educator implements Teacher{
+    LEON,
+    HUNTER;
+
+
+
+    @Override
+    public void teach(Learner learner, double numberOfHours) {
+
+    }
+
+    @Override
+    public void lecture(Learner[] learners, double numberOfHours) {
+
+    }
+}
